6d576c22d236c6dacc35e171ef3924599e8067cf
bodajava <bbido761@gmail.com.com>
2026-03-28 04:17:18 +0200

    chore: prepare project for deployment, add docs, fix entry point, and update gitignore

--- src/app.bootstrap.js
+++ src/app.bootstrap.js
@@ -1,3 +1,6 @@
+import AgentAPI from 'apminsight'
+AgentAPI.config();
+
 import express from 'express'
 import { MatchesRouter } from './Module/index.js';
 import { connectDB } from './db/connection.js';
